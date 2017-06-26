73686c45dcbd18847de683bc69759064eb8bfec3
DESKTOP-MOBHA9O\Maxime <maxdu667@hotmail.fr>
2017-06-26 12:02:16 +0200

    Authentication async

---
+++ AuthNDecryptService/AuthNDecryptService/AuthNDecryptService.svc.cs
@@ -0,0 +1,199 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthNDecryptService
+{
+    // REMARQUE : vous pouvez utiliser la commande Renommer du menu Refactoriser pour changer le nom de classe "AuthNDecryptService" à la fois dans le code, le fichier svc et le fichier de configuration.
+    // REMARQUE : pour lancer le client test WCF afin de tester ce service, sélectionnez AuthNDecryptService.svc ou AuthNDecryptService.svc.cs dans l'Explorateur de solutions et démarrez le débogage.
+    public class AuthNDecryptService : IAuthNDecryptService
+    {
+        private string tokenApp = "KwdcVAQVK0KzabAM62qp0g==";
+        private User user = new User();
+
+        //public string Authenticate(User user)
+        //{
+        //    bool isAllowed = false;
+        //    Console.WriteLine("test");
+
+        //    using (var cnn = new MySqlConnection(Properties.Settings.Default.DBConnectionString))
+        //    {
+        //        using (var cmd = new MySqlCommand(
+        //            "SELECT id_user, username, password FROM user ", cnn))
+        //        {
+        //            cnn.Open();
+        //            using (MySqlDataReader dataReader = cmd.ExecuteReader())
+        //            {
+        //                if (dataReader.HasRows)
+        //                {
+        //                    while (dataReader.Read())
+        //                    {
+        //                        for (int i = 0; i < dataReader.FieldCount; i = i + 3)
+        //                        {
+        //                            if (user.username == dataReader.GetString(i+1) && user.userPassword == dataReader.GetString(i + 2))
+        //                            {
+        //                                isAllowed = true;
+        //                                user.userId = Convert.ToInt32(dataReader.GetString(i));
+
+        //                                user.tokenUser += user.username.GetHashCode();
+        //                                user.tokenUser += user.userPassword.GetHashCode();
+        //                                user.tokenUser += user.tokenApp.GetHashCode();
+        //                                user.tokenUser += DateTime.Now.GetHashCode();
+        //                                user.tokenUser = user.tokenUser.GetHashCode().ToString();
+        //                                this.user = user;
+        //                                break;
+        //                            }
+        //                        }
+        //                    }
+        //                }
+        //            }
+        //        }
+        //        if (isAllowed)
+        //        {
+        //            int rowsChanged = 0;
+        //            using (var cmd = new MySqlCommand(
+        //                "UPDATE user " +
+        //                "SET token_user = @tokenUser " +
+        //                "WHERE id_user = @userId", cnn))
+        //            {
+        //                cmd.Parameters.Add(new MySqlParameter("@tokenUser", user.tokenUser));
+        //                cmd.Parameters.Add(new MySqlParameter("@userId", user.userId));
+        //                rowsChanged = (int)cmd.ExecuteNonQuery();
+        //                if(rowsChanged != 0)
+        //                {
+        //                    return user.tokenUser;
+        //                }
+        //            }
+        //        }
+        //    }
+
+        //    return ("false");
+        //}
+
+        public async Task<string> AuthenticateAsync(User user)
+        {
+            //throw new NotImplementedException();
+            bool isAllowed = false;
+            Console.WriteLine("test");
+
+            using (var cnn = new MySqlConnection(Properties.Settings.Default.DBConnectionString))
+            {
+                using (var cmd = new MySqlCommand(
+                    "SELECT id_user, username, password FROM user ", cnn))
+                {
+                    cnn.Open();
+                    using (MySqlDataReader dataReader = cmd.ExecuteReader())
+                    {
+                        if (dataReader.HasRows)
+                        {
+                            while (dataReader.Read())
+                            {
+                                for (int i = 0; i < dataReader.FieldCount; i = i + 3)
+                                {
+                                    if (user.username == dataReader.GetString(i + 1) && user.userPassword == dataReader.GetString(i + 2))
+                                    {
+                                        isAllowed = true;
+                                        user.userId = Convert.ToInt32(dataReader.GetString(i));
+
+                                        user.tokenUser += user.username.GetHashCode();
+                                        user.tokenUser += user.userPassword.GetHashCode();
+                                        user.tokenUser += user.tokenApp.GetHashCode();
+                                        user.tokenUser += DateTime.Now.GetHashCode();
+                                        user.tokenUser = user.tokenUser.GetHashCode().ToString();
+                                        this.user = user;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                if (isAllowed)
+                {
+                    int rowsChanged = 0;
+                    using (var cmd = new MySqlCommand(
+                        "UPDATE user " +
+                        "SET token_user = @tokenUser " +
+                        "WHERE id_user = @userId", cnn))
+                    {
+                        cmd.Parameters.Add(new MySqlParameter("@tokenUser", user.tokenUser));
+                        cmd.Parameters.Add(new MySqlParameter("@userId", user.userId));
+                        rowsChanged = (int)cmd.ExecuteNonQuery();
+                        if (rowsChanged != 0)
+                        {
+                            return user.tokenUser;
+                        }
+                    }
+                }
+            }
+
+            return ("false");
+        }
+
+        public User GetUser(int loginId)
+        {
+            User user = new User();
+            using (var cnn = new MySqlConnection(Properties.Settings.Default.DBConnectionString))
+            {
+                using (var cmd = new MySqlCommand(
+                    "SELECT username, password, token_user FROM user " +
+                    "WHERE id_user = @loginId", cnn))
+                {
+                    cmd.Parameters.Add(new MySqlParameter("@loginId", loginId));
+
+                    cnn.Open();
+                    using (MySqlDataReader dataReader = cmd.ExecuteReader())
+                    {
+                        if (dataReader.HasRows)
+                        {
+                            while (dataReader.Read())
+                            {
+                                user.username = dataReader.GetString(0);
+                                user.userPassword = dataReader.GetString(1);
+                                user.tokenUser = dataReader.GetString(2);
+                            }
+                        }
+                    }
+                }
+            }
+            return user;
+        }
+
+        //public Task<User> GetUserAsync(int loginId)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public bool SendDocument(Document document, string tokenApp, string tokenUser)
+        {
+            if (this.user != new User() && tokenApp == this.tokenApp && tokenUser == GetUser(user.userId).tokenUser)
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        //public Task<bool> SendDocumentAsync(Document document, string tokenApp, string tokenUser)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        public string tokenify(User user)
+        {
+            string tokenUser = string.Empty;
+            tokenUser += user.username.GetHashCode();
+            tokenUser += user.userPassword.GetHashCode();
+            tokenUser += user.tokenApp.GetHashCode();
+            tokenUser += DateTime.Now.GetHashCode();
+            tokenUser = user.tokenUser.GetHashCode().ToString();
+
+            return user.tokenUser;
+        }
+    }
+}
---
+++ AuthNDecryptService/AuthNDecryptService/IAuthNDecryptService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthNDecryptService
+{
+    [ServiceContract]
+    public interface IAuthNDecryptService
+    {
+        //[OperationContract]
+        //string Authenticate(User user);
+
+        [OperationContract]
+        Task<string> AuthenticateAsync(User user);
+
+        [OperationContract]
+        User GetUser(int loginId);
+
+        //[OperationContract]
+        //Task<User> GetUserAsync(int loginId);
+
+        [OperationContract]
+        bool SendDocument(Document document, String tokenApp, String tokenUser);
+
+        //[OperationContract]
+        //Task<bool> SendDocumentAsync(Document document, String tokenApp, String tokenUser);
+    }
+
+
+    [DataContract]
+    public class User
+    {
+        [DataMember]
+        public int userId { get; set; }
+        [DataMember]
+        public string username { get; set; }
+        [DataMember]
+        public string userPassword { get; set; }
+        [DataMember]
+        public string tokenApp { get; set; }
+        [DataMember]
+        public string tokenUser { get; set; }
+
+    }
+
+    [DataContract]
+    public class Document
+    {
+        [DataMember]
+        public string fileName { get; set; }
+        [DataMember]
+        public string fileContent { get; set; }
+        [DataMember]
+        public string decryptKey { get; set; }
+
+    }
+}
